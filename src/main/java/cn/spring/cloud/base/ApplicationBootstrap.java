6cc3af72092a92c8749bfc5b3881785416f4242d
刘永超 <liuyongchao@mofanghr.com>
2018-08-29 21:04:26 +0800

    去掉component scan

--- src/main/java/cn/spring/cloud/base/ApplicationBootstrap.java
+++ src/main/java/cn/spring/cloud/base/ApplicationBootstrap.java
@@ -2,10 +2,8 @@ package cn.spring.cloud.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("cn.spring")
 public class ApplicationBootstrap {
 
 	public static void main(String[] args) {
